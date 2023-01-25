b0a09e228816729271e1a445cf2c6bb210af61e4
Chaewony <tcd1205@naver.com>
2023-01-25 23:21:48 +0900

    위에서 아래로
    p.178

---
+++ Sorting/위에서 아래로.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    vector<int> vec;
+    for (int i = 0; i < n; i++) {
+        int m;
+        cin >> m;
+        vec.push_back(m);
+    }
+
+    sort(vec.rbegin(), vec.rend());
+
+    for (auto i : vec) {
+        cout << i << " ";
+    }
+}
