3d941fc63b426ada1e9ea826d1107589a4a4ffa9
Chaewony <tcd1205@naver.com>
2023-01-25 23:22:49 +0900

    성적이 낮은 순서로 학생 출력하기
    p.180

---
+++ Sorting/성적이 낮은 순서로 학생 출력하기.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+bool compare(pair<string, int>a, pair<string, int>b)
+{
+    return a.second > b.second;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    vector<pair<string, int>> vec;
+    for (int i = 0; i < n; i++) {
+        string str;
+        int m;
+        cin >> str >> m;
+        vec.push_back({ str, m });
+    }
+
+    sort(vec.rbegin(), vec.rend(), compare);
+
+    for (auto i : vec) {
+        cout << i.first << " ";
+    }
+}
