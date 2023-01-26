fceb19d935ca97755cc580b6bfb7696e62c1cef2
Chaewony <tcd1205@naver.com>
2023-01-26 14:53:45 +0900

    두 배열의 원소 교체
    p.182

---
+++ Sorting/두 배열의 원소 교체.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+//입력부
+    long long n, m;
+    cin >> n >> m;
+
+    vector<long long> A;
+    vector<long long> B;
+
+    long long tmp;
+
+    for (int j = 0; j < n; j++) {
+        cin >> tmp;
+        A.push_back(tmp);
+    }
+
+    for (int j = 0; j < n; j++) {
+        cin >> tmp;
+        B.push_back(tmp);
+    }
+
+//계산부
+    sort(A.begin(), A.end());
+    sort(B.rbegin(), B.rend());
+
+    long long result = 0;
+    long long index = 0;
+    for (int i = 0; i < n; i++) {
+        if (A[i] < B[index] && index < m) {
+            result += B[index];
+            index++;
+        }
+        else
+            result += A[i];
+    }
+
+    cout << result;
+}
