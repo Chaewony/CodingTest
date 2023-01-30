2dd7f27c78981c3e97bbbd8f6bb1cbf80985d629
Chaewony <tcd1205@naver.com>
2023-01-30 19:39:29 +0900

    정렬된 배열에서 특정 수의 개수 구하기
    p.367

---
+++ BinarySearch/정렬된 배열에서 특정 수의 개수 구하기.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    long long n, m; 
+    cin >> n >> m;
+
+    vector<long long> cards;
+
+    for (long long i = 0; i < n; i++) {
+        long long tmp;
+        cin >> tmp;
+        cards.push_back(tmp);
+    }
+
+    sort(cards.begin(), cards.end());
+
+    long long result = upper_bound(cards.begin(), cards.end(), m) - lower_bound(cards.begin(), cards.end(), m);
+
+    if (result == 0)
+        result = -1;
+
+    cout << result;
+}
