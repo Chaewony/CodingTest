0760ab331e559322fb98c2c746deab431b795e02
Chaewony <76686576+Chaewony@users.noreply.github.com>
2023-02-05 03:15:40 +0900

    [Silver III] Title: 1로 만들기, Time: 12 ms, Memory: 14320 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    long long n;
+    cin >> n;
+
+    vector<long long> vec;
+
+    vec.push_back(0); //인덱스 맞추기 위해 v[0] = 0
+    vec.push_back(0); //v[1] = 0
+    vec.push_back(1); //v[2] = 1
+    vec.push_back(1); //v[3] = 1
+
+    for (long long i = 4; i < n + 1; i++) {
+        
+        long long min = 1000001;
+
+        if (i % 3 == 0) {
+            min = 1 + vec[i / 3];
+        }
+        if (i % 2 == 0) { 
+            if (min > 1 + vec[i / 2])
+                min = 1 + vec[i / 2];
+        }
+        if (min > 1 + vec[i - 1])
+            min = 1 + vec[i - 1];
+        
+        vec.push_back(min);
+    }
+
+    cout << vec[n];
+}
